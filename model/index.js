9711c54d614103123d8a62e3ce2d3d8e4371fc56
Name <nizaam01.jeftha@gmail.com>
2024-08-16 11:15:45 +0200

    update2.0

--- model/index.js
+++ model/index.js
@@ -1,8 +1,9 @@
 import { Users } from "../Users";
 import { Products } from "./Products";
+
 const users = new Users()
 const products = new Products()
 export { 
-    users,
-    products
+    Users,
+    Products
 }
